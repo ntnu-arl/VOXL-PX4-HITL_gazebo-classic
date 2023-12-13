b77001e4338d9dadc61d8ebd9cb1bd68e248985d
Eric Katzfey <eric.katzfey@modalai.com>
2023-12-13 14:59:45 -0800

    Added new convenience script

---
+++ run_gazebo_vio.bash
@@ -0,0 +1,15 @@
+#!/bin/bash
+#
+# Run gazebo in support of voxl2 hitl
+#
+
+# setup Gazebo env and update package path
+export GAZEBO_PLUGIN_PATH=$GAZEBO_PLUGIN_PATH:$(pwd)
+export GAZEBO_MODEL_PATH=$GAZEBO_MODEL_PATH:$(pwd)/models
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$(pwd)/build
+
+echo -e "GAZEBO_PLUGIN_PATH $GAZEBO_PLUGIN_PATH"
+echo -e "GAZEBO_MODEL_PATH $GAZEBO_MODEL_PATH"
+echo -e "LD_LIBRARY_PATH $LD_LIBRARY_PATH"
+
+gazebo worlds/hitl_iris_vio.world
