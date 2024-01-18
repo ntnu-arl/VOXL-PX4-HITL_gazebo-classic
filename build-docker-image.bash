47255857b7279a14473555a622bb6d71ab7b2edd
Eric Katzfey <eric.katzfey@modalai.com>
2024-01-18 08:46:50 -0800

    Move to v1.2

--- build-docker-image.bash
+++ build-docker-image.bash
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 # The no cache option will force everything to be rebuilt from scratch
-# docker build --no-cache -t voxl-gazebo-docker:v1.1 .
+# docker build --no-cache -t voxl-gazebo-docker:v1.2 .
 
-docker build -t voxl-gazebo-docker:v1.1 .
+docker build -t voxl-gazebo-docker:v1.2 .
+docker tag voxl-gazebo-docker:v1.2 voxl-gazebo-docker:latest
 
